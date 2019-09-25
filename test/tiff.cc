175c05286263fd4c45f62a0b77e54fa56687212a
Jörgen Grahn <grahn+src@snipabacken.se>
2019-09-25 21:31:18 +0200

    more tiff test

--- test/tiff.cc
+++ test/tiff.cc
@@ -23,11 +23,10 @@ namespace tiff {
 
     namespace simple {
 
-	void bigendian(orchis::TC)
-	{
-	    const auto app1 = h(
+	namespace data {
+	    const auto bigendian = h(
 		"45 78 69 66 00 00" // Exif
-		"4d4d 2a 0000 0008" // TIFF header
+		"4d4d 002a 0000 0008" // TIFF header
 		"0001"              // IFD 0 with one tag
 		"8769 0004 00000001 0000 001a" // Exif IFD pointer
 		"0000 0000"         // no next IFD
@@ -37,7 +36,28 @@ namespace tiff {
 		"0000 0000"         // no next IFD
 		"32 30 31 39 3a 30 39 3a 31 32 20 32 32 3a 33 30 3a 30 30 00");
 
-	    File f {app1};
+	    const auto lendian = h(
+		"45 78 69 66 00 00" // Exif
+		"4949 2a00 0800 0000" // TIFF header
+		"0100"              // IFD 0 with one tag
+		"6987 0400 01000000 1a00 0000" // Exif IFD pointer
+		"0000 0000"         // no next IFD
+		""
+		"0100"              // Exif IFD with one tag
+		"0390 0200 14000000 2c00 0000" // DateTimeOriginal
+		"0000 0000"         // no next IFD
+		"32 30 31 39 3a 30 39 3a 31 32 20 32 32 3a 33 30 3a 30 30 00");
+	}
+
+	void bigendian(orchis::TC)
+	{
+	    File f {data::bigendian};
+	}
+
+	void lendian(orchis::TC)
+	{
+	    File f {data::lendian};
+	    f.ifd0.find(0x8769, 4);
 	}
     }
 }
