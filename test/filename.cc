863f9a3ee3b401170afe41cdc99f22b713e3cb0b
Jörgen Grahn <grahn+src@snipabacken.se>
2019-10-23 20:31:50 +0200

    clang fix
    I'm assuming clang++ 7 is right to warn; it was a name
    lookup issue.

--- test/filename.cc
+++ test/filename.cc
@@ -2,52 +2,49 @@
 
 #include <filename.h>
 
-namespace {
-    bool operator== (const Serial&a, int b) { return a == Serial(b); }
-    bool operator== (int a, const Serial& b) { return b==a; }
-}
-
 namespace filename {
 
     using orchis::TC;
     using orchis::assert_eq;
 
+    const Serial s1234{1234};
+
     void olympus(TC)
     {
-	assert_eq(serial("pa051234.jpg"), 1234);
-	assert_eq(serial("PA051234.JPG"), 1234);
+	assert_eq(serial("pa051234.jpg"), s1234);
+	assert_eq(serial("PA051234.JPG"), s1234);
     }
 
     void dsc(TC)
     {
-	assert_eq(serial("dsc01234.jpg"), 1234);
-	assert_eq(serial("DSC01234.JPG"), 1234);
+	assert_eq(serial("dsc01234.jpg"), s1234);
+	assert_eq(serial("DSC01234.JPG"), s1234);
     }
 
     void dscn(TC)
     {
-	assert_eq(serial("dscn1234.jpg"), 1234);
-	assert_eq(serial("DSCN1234.JPG"), 1234);
+	assert_eq(serial("dscn1234.jpg"), s1234);
+	assert_eq(serial("DSCN1234.JPG"), s1234);
     }
 
     void imag(TC)
     {
-	assert_eq(serial("imag1234.jpg"), 1234);
-	assert_eq(serial("IMAG1234.JPG"), 1234);
+	assert_eq(serial("imag1234.jpg"), s1234);
+	assert_eq(serial("IMAG1234.JPG"), s1234);
     }
 
     void self(TC)
     {
-	assert_eq(serial("1990-01-02_1234.jpg"), 1234);
+	assert_eq(serial("1990-01-02_1234.jpg"), s1234);
     }
 
     void path(TC)
     {
-	assert_eq(1234, serial("pa051234.jpg"));
-	assert_eq(1234, serial("foo/pa051234.jpg"));
-	assert_eq(1234, serial("foo/bar/pa051234.jpg"));
-	assert_eq(1234, serial("./pa051234.jpg"));
-	assert_eq(1234, serial(".//pa051234.jpg"));
-	assert_eq(1234, serial("/pa051234.jpg"));
+	assert_eq(s1234, serial("pa051234.jpg"));
+	assert_eq(s1234, serial("foo/pa051234.jpg"));
+	assert_eq(s1234, serial("foo/bar/pa051234.jpg"));
+	assert_eq(s1234, serial("./pa051234.jpg"));
+	assert_eq(s1234, serial(".//pa051234.jpg"));
+	assert_eq(s1234, serial("/pa051234.jpg"));
     }
 }
