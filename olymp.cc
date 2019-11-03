8db6cf92612bc5004637f5b76a550d7e66ff706d
Jörgen Grahn <grahn+src@snipabacken.se>
2019-11-03 21:46:49 +0100

    olymp 3.4

--- olymp.cc
+++ olymp.cc
@@ -283,7 +283,7 @@ int main(int argc, char ** argv)
 	    return 0;
 	    break;
 	case 'V':
-	    std::cout << "olymp 3.3\n"
+	    std::cout << "olymp 3.4\n"
 		      << "Copyright (c) 2002--2012, 2014, 2019 Jörgen Grahn\n";
 	    return 0;
 	    break;
